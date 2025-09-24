64fe3ac67e0db77ac3394a6cb454005da84d378c
Mayomi <ayandiranmayomi@gmail.com>
2025-09-24 15:51:55 +0100

    Initial Commit

---
+++ Rewrite/RewriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewriteApp.swift
+//  Rewrite
+//
+//  Created by Mayomi Ayandiran on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RewriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
